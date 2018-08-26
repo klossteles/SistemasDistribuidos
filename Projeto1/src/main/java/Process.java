2a8f58aac1ecb21d42d72cc1daeb71ee03c68fae
brendon <brendonpasquim@gmail.com>
2018-08-26 14:25:34 -0300

    Criação classe RSA com métodos estáticos. Alteração de versão do JRE de 1.5 para 1.8 (POM).

--- Projeto1/src/main/java/Process.java
+++ Projeto1/src/main/java/Process.java
@@ -1,52 +1,56 @@
+
+import criptografia.RSA;
 import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Process extends Thread {
+
     private int state;
     private Key publicKey;
     private Key privateKey;
     private final MulticastSocket socket;
     private final InetAddress group;
     private Map<String, Key> processosConhecidos;
+    
+    /**
+     * Utilizado para definir o encoding do texto das mensagens enviadas. 
+     * Necessário para efetuar a descriptografia adequada das mensagens trocadas.
+     */
+    private final Charset encodingPadrao;
 
-    public Process(int state, InetAddress group, MulticastSocket socket) {
+    public Process(int state, InetAddress group, MulticastSocket socket){
         this.state = state;
         this.socket = socket;
         this.group = group;
         this.processosConhecidos = new HashMap<>();
-        try{
-            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-            kpg.initialize(2048);
-            KeyPair kp = kpg.generateKeyPair();
-            this.publicKey = kp.getPublic();
-            this.privateKey = kp.getPrivate();
-        }catch (NoSuchAlgorithmException e){
-            e.printStackTrace();
-        }
+        this.encodingPadrao = StandardCharsets.UTF_8;
+        
+        KeyPair kp = RSA.gerarChavePublicaPrivada();
+        this.publicKey = kp.getPublic();
+        this.privateKey = kp.getPrivate();
     }
 
     public void announce(){
 
     }
 
-    private void criptografar (InputStream input) {
+    private void criptografar(InputStream input){
 
     }
 
     @Override
-    public void run() {
+    public void run(){
         byte[] buffer = new byte[1000];
         DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
-        while (true){
+        while(true){
 
         }
     }
--- Projeto1/src/main/java/criptografia/RSA.java
+++ Projeto1/src/main/java/criptografia/RSA.java
@@ -1,20 +1,92 @@
+package criptografia;
+
+import java.nio.charset.Charset;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.PrivateKey;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
-public class RSA{
-    public static byte[] criptografar(PrivateKey privateKey, String message){
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+public class RSA {
+
+    /**
+     * Gera uma par de chaves Públic/Privada utilizando o algoritmo RSA.
+     * 
+     * @return [java.security.KeyPair] contendo um par de chaves Pública e Privada.
+     */
+    public static KeyPair gerarChavePublicaPrivada(){
+        KeyPairGenerator kpg;
         
-        return cipher.doFinal(message.getBytes());
+        try {
+            kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(2048);
+        } catch(NoSuchAlgorithmException ex) {
+            Logger.getLogger(RSA.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Falha ao Gerar par de Chaves Pública/Privada com algoritmo RSA.");
+        }
+        
+        return kpg.generateKeyPair();
     }
 
-    public static descriptografar(PublicKey publicKey, byte[] data){
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+    /**
+     * Criptografa uma mensagem em texto puro com uma chave Privada RSA.
+     * 
+     * @param privateKey [Obrigatório] - Chave Privada para criptografia.
+     * @param message    [Obrigatório] - Mensagem que será criptografada.
+     * 
+     * @return [byte[]] contendo a mensagem criptografada.
+     */
+    public static byte[] criptografar(PrivateKey privateKey, String message){
+        if(privateKey == null || message == null){
+            return null;
+        }
+        
+        Cipher cipher;
+        byte[] dadosCriptografados = null;
 
-        return cipher.doFinal(data);
+        try {
+            cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+            dadosCriptografados = cipher.doFinal(message.getBytes());
+        } catch(NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
+            Logger.getLogger(RSA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return dadosCriptografados;
     }
 
+    /**
+     * Descriptografa um conjunto de bytes com uma chave pública RSA.
+     * 
+     * @param publicKey [Obrigatório] - Chave Pública para descriptografia.
+     * @param data      [Obrigatório] - Array de Bytes que serão descriptografados.
+     * @param encode    [Obrigatório] - O encoding da mensagem original. Necessário para conversão entre bytes para String.
+     * 
+     * @return [java.lang.String] contendo a mensagem descriptografada.
+     */
+    public static String descriptografar(PublicKey publicKey, byte[] data, Charset encode){
+        if(publicKey == null || data == null || encode == null){
+            return null;
+        }
+        
+        Cipher cipher;
+        byte[] dadosDescriptografados = null;
+
+        try {
+            cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, publicKey);
+            dadosDescriptografados = cipher.doFinal(data);
+        } catch(NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException ex) {
+            Logger.getLogger(RSA.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return new String(dadosDescriptografados, encode);
+    }
 }
